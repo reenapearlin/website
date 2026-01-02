c9aa183a44662a752bb3612de1ee28aea543b92f
reenapearlin <reenapearlin.pro@gmail.com>
2026-01-02 16:14:50 +0530

    Added cart details and enhanced UI

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-let cartCount = 0;
+let cart = [];
+let total = 0;
 
-function addToCart(productName) {
-    cartCount++;
-    document.getElementById("cart-count").innerText = cartCount;
-    alert(productName + " added to cart!");
+function addToCart(product, price) {
+    cart.push({ product, price });
+    total += price;
+
+    document.getElementById("cart-count").innerText = cart.length;
+    document.getElementById("total-price").innerText = total;
+
+    const li = document.createElement("li");
+    li.innerText = `${product} - ₹${price}`;
+    document.getElementById("cart-items").appendChild(li);
+}
+
+function clearCart() {
+    cart = [];
+    total = 0;
+    document.getElementById("cart-count").innerText = 0;
+    document.getElementById("total-price").innerText = 0;
+    document.getElementById("cart-items").innerHTML = "";
 }
